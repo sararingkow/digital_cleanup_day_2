f989e64990c5d7964444b3d2d376c4266b1782c0
Sara <sape0003@stud.kea.dk>
2024-09-17 11:52:31 +0200

    Some things updated

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,13 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [
-        require('@tailwindcss/typography'),
-        require('@tailwindcss/forms'),
-        require('@tailwindcss/aspect-ratio'),
-        require('@tailwindcss/container-queries'),
-    ],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        "light-green": "#798E7F",
+        "dark-green": "#23483B",
+        "off-white": "#F7F7F5",
+        "light-yellow": "#FFBB00",
+        "medium-yellow": "#FFCB3F",
+        "dark-yellow": "#C89300",
+      },
+      plugins: [
+        require("@tailwindcss/typography"),
+        require("@tailwindcss/forms"),
+        require("@tailwindcss/aspect-ratio"),
+        require("@tailwindcss/container-queries"),
+      ],
+    },
+  },
+};
