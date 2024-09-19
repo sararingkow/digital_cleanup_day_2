c879b022488acd3d56b96f409c4ca2c649726bb2
Sara <sape0003@stud.kea.dk>
2024-09-19 16:22:50 +0200

    Changes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -11,6 +11,28 @@ export default {
         "medium-yellow": "#FFCB3F",
         "dark-yellow": "#C89300",
       },
+
+      fontSize: {
+        brodtekst_mini: "1rem",
+        brodtekst_grid: "1.25rem",
+        brodtekst: "1.375rem",
+        h4: "1.5rem",
+        menu: "1.75rem",
+        formular: "2rem",
+        h3: "2.5rem",
+        h2: "3rem",
+        h1: "7.5rem",
+
+        brodtekst_mini_m: "0.875rem",
+        brodtekst_grid_m: "1rem",
+        brodtekst_m: "1rem",
+        h4_m: "1.375rem",
+        menu_m: "1.25rem",
+        formular_m: "1.375rem",
+        h3_m: "1.625rem",
+        h2_m: "2rem",
+        h1_m: "3.5rem",
+      },
       plugins: [
         require("@tailwindcss/typography"),
         require("@tailwindcss/forms"),
